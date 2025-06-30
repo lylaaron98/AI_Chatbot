b5d4304a8049717380ab5aea0c1a10e737288fb5
Aaron Lee <lyl.aaron.98@gmail.com>
2025-07-01 00:32:50 +0800

    update dependencies
    changed to groqai instead and updated package dependencies

--- chatbot_ai.py
+++ chatbot_ai.py
@@ -1,21 +1,16 @@
 import streamlit as st
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from groq_config import get_groq_llm
 
-st.title("AIE/ASE Personal Assistant ChatApp")
+st.title("AIE/ASE Personal Assistant")
 
-# Add a rainbow line below the header
-st.markdown(
-    """
+st.markdown("""
     <div style='height:6px; background: linear-gradient(90deg, red, orange, yellow, green, blue, indigo, violet); border-radius: 3px; margin-bottom: 20px;'></div>
-    """,
-    unsafe_allow_html=True
-)
+""", unsafe_allow_html=True)
 
-#initialize chat history
 if "messages" not in st.session_state:
-    st.session_state.messages = []
+    st.session_state.messages = [SystemMessage(content="Personal Assistant:")]
 
-#display chat message from history on app rerun
 for message in st.session_state.messages:
     if isinstance(message, HumanMessage):
         with st.chat_message("user"):
@@ -24,17 +19,19 @@ for message in st.session_state.messages:
         with st.chat_message("assistant", avatar="images/PA_image.jpg"):
             st.markdown(message.content)
 
-#create the input form
 prompt = st.chat_input("Enter a prompt")
 
-#if input provided, add it to the screen
 if prompt:
     with st.chat_message("user"):
         st.markdown(prompt)
-        st.session_state.messages.append(HumanMessage(prompt))
-    # echo (response) and add it to the screen
-    response = f"Echo: {prompt}"
+    st.session_state.messages.append(HumanMessage(prompt))
+
+    llm = get_groq_llm()
+
+    result = llm.invoke(st.session_state.messages).content
 
     with st.chat_message("assistant", avatar="images/PA_image.jpg"):
-        st.markdown(response)
-        st.session_state.messages.append(AIMessage(response))
+        st.markdown("""
+            <span style='font-weight: bold; color: #ff9800; font-size: 1.15em;'>Personal Assistant:</span> {result}
+        """.format(result=result), unsafe_allow_html=True)
+    st.session_state.messages.append(AIMessage(result))
