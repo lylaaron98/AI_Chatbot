a240041ec676cfee5bfd6034f6e24e84c7aa1929
Aaron Lee <lyl.aaron.98@gmail.com>
2025-07-04 11:17:33 +0800

    Updated FAQ
    added the FAQ indicating the difference between AIE and ASE

--- faq_config.py
+++ faq_config.py
@@ -9,6 +9,8 @@ FAQ = {
     "What is ASE?": "The ASE design and deliver dynamic, empowering experiences that create the right conditions to unleash group genius. Using our unique combination of capabiltiies, we strip away the rational, emtional and political blockers that get in the way of powerful, transformative business outcomes. By unlocking this human potential, we enable teams, projects and organisations to connect differently. This creates the passion, engagement, alignment and momentum you need to get the future you want.",
     "Can I use AIE?": "The room is available from 9:00 AM to 6:00 PM, Monday to Friday.",
     "What do I need to provide if I want to use the AIE & ASE service?" : "To use the AIE & ASE service, you need to provide a brief description of your project or idea, the number of participants, and any specific requirements you may have for the session.",
+    "What is the difference between AIE and ASE?": "The AIE focuses on innovation by providing a platform to explore, experiment, and de-risk new ideas and technologies. On the other hand, the ASE specializes in creating dynamic, collaborative experiences to unlock group potential and drive transformative business outcomes. While AIE is centered on innovation enablement, ASE is focused on facilitating human-centered collaboration.",
+    
 }
 
 faq_questions = list(FAQ.keys())
